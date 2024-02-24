2c4ed64090f4ee01c45b9d73f416b31051dce6ed
Vishwas verma <project09cdac23prv@gmail.com>
2024-02-25 03:21:31 +0530

    25-02-2024 03:21 AM

--- BackEND/Spring/DrugDesributionManagementSystem/DrugDesributionManagementSystem/src/main/java/com/cdac_project/repository/OrderRepository.java
+++ BackEND/Spring/DrugDesributionManagementSystem/DrugDesributionManagementSystem/src/main/java/com/cdac_project/repository/OrderRepository.java
@@ -3,6 +3,8 @@ package com.cdac_project.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.cdac_project.model.Order;
@@ -10,4 +12,7 @@ import com.cdac_project.model.Order;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
     List<Order> findByPharmacistId(int pharmacistId);
+    
+    @Query("SELECT o FROM Order o WHERE o.pharmacistId = :pharmacistID")
+    public List<Order> getPharmacistOrders(@Param("pharmacistID") int id);
 }
