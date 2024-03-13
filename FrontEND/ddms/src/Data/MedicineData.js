04a260077df2894867e1cc11e205b5e6e1ea35e7
Vishwas verma <project09cdac23prv@gmail.com>
2024-03-13 20:10:03 +0530

    Entered More Medicine Data

--- FrontEND/ddms/src/Data/MedicineData.js
+++ FrontEND/ddms/src/Data/MedicineData.js
@@ -1,13 +1,53 @@
+import amitriptyline from '../Assets/Image/Amitriptyline.jpg';
 import amoxicillin from '../Assets/Image/Amoxicillin.jpg';
 import aspirin from '../Assets/Image/Aspirin.jpeg';
+import atorvastatin from '../Assets/Image/Atorvastatin.jpg';
+import bupropion from '../Assets/Image/Bupropion.jpg';
+import cephalexin from '../Assets/Image/Cephalexin.jpg';
 import cetirizine from '../Assets/Image/Cetirizine.jpeg';
+import chlorphenamine from '../Assets/Image/Chlorphenamine.jpg';
 import ciclesonide from '../Assets/Image/Ciclesonide.jpg';
 import citalopram from '../Assets/Image/Citalopram.jpg';
+import clarithromycin from '../Assets/Image/Clarithromycin.jpg';
+import clobetasolPropionate from '../Assets/Image/ClobetasolPropionate.jpg';
+import doxycycline from '../Assets/Image/Doxycycline.jpg';
+import doxylamine from '../Assets/Image/Doxylamine.jpg';
+import duloxetine from '../Assets/Image/Duloxetine.jpg';
+import escitalopram from '../Assets/Image/Escitalopram.jpg';
+import famotidine from '../Assets/Image/Famotidine.jpg';
+import fluconazole from '../Assets/Image/Fluconazole.jpg';
+import fluoxetine from '../Assets/Image/Fluoxetine.jpg';
+import fluticasonePropionate from '../Assets/Image/FluticasonePropionate.jpg';
+import furosemide from '../Assets/Image/Furosemide.jpeg';
+import hydrocodone from '../Assets/Image/Hydrocodone.jpeg';
+import hydrocortisone from '../Assets/Image/Hydrocortisone.jpg';
+import ibuprofen from '../Assets/Image/Ibuprofen.jpg';
+import insulin from '../Assets/Image/Insulin.jpg';
+import levothyroxine from '../Assets/Image/Levothyroxine.jpg';
+import loperamide from '../Assets/Image/Loperamide.jpg';
 import loratadine from '../Assets/Image/Loratadine.jpg';
+import metformin from '../Assets/Image/Metformin.jpg';
+import metoclopramide from '../Assets/Image/Metoclopramide.jpg';
+import mirtazapine from '../Assets/Image/Mirtazapine.jpg';
+import mupirocin from '../Assets/Image/Mupirocin.jpg';
+import nortriptyline from '../Assets/Image/Nortriptyline.jpg';
+import nystatin from '../Assets/Image/Nystatin.jpg';
 import omeprazole from '../Assets/Image/Omeprazole.jpg';
+import ondansetron from '../Assets/Image/Ondansetron.jpg';
+import oxycodone from '../Assets/Image/Oxycodone.jpg';
+import pantoprazole from '../Assets/Image/Pantoprazole.jpeg';
 import paracetamol from '../Assets/Image/Paracetamol.jpg';
 import penicillin from '../Assets/Image/Penicillin.jpeg';
+import prednisone from '../Assets/Image/Prednisone.jpeg';
 import ranitidine from '../Assets/Image/Ranitidine.jpeg';
+import salicylicAcid from '../Assets/Image/SalicylicAcid.jpg';
+import sertraline from '../Assets/Image/Sertraline.jpg';
+import sildenafil from '../Assets/Image/Sildenafil.jpeg';
+import simvastatin from '../Assets/Image/Simvastatin.jpeg';
+import tadalafil from '../Assets/Image/Tadalafil.jpeg';
+import tramadol from '../Assets/Image/Tramadol.jpg';
+import trazodone from '../Assets/Image/Trazodone.jpg';
+import venlafaxine from '../Assets/Image/Venlafaxine.jpg';
 
 
 export const MedicineData =  [
@@ -122,15 +162,466 @@ export const MedicineData =  [
         rating: 4,
     },
     {
-        "id": 12,
-        "name": "Aspirin",
-        "img": aspirin,
-        "category": 'Emergency_Condition',
-        "quantity": 100,
-        "desc": "2023-01-01 00:00:00",
-        "price": 15,
-        "rating": 4.9
+        id: 12,
+        name: 'Aspirin',
+        img: aspirin,
+        category: 'Emergency_Condition',
+        quantity: 100,
+        desc: '2023-01-01 00:00:00',
+        price: 15,
+        rating: 4.9
+    },
+    {
+        id: 13,
+        name: 'Cephalexin',
+        img:  cephalexin ,
+        category: 'Antibiotics',
+        quantity: 2,
+        desc: '2024-01-22 00:00:00',
+        price: 17,
+        rating: 3
+    },
+    {
+        id: 14,
+        name: 'Pantoprazole',
+        img:  pantoprazole ,
+        category: 'Antacids',
+        quantity: 205,
+        desc: '2024-01-21 00:00:00',
+        price: 12,
+        rating: 0
+    },
+    {
+        id: 15,
+        name: 'Hydrocodone',
+        img:  hydrocodone ,
+        category: 'Painkillers',
+        quantity: 215,
+        desc: '2024-01-20 00:00:00',
+        price: 10,
+        rating: 0
+    },
+    {
+        id: 16,
+        name: 'Clarithromycin',
+        img:  clarithromycin ,
+        category: 'Antibiotics',
+        quantity: 225,
+        desc: '2024-01-19 00:00:00',
+        price: 19,
+        rating: 2
+    },
+    {
+        id: 17,
+        name: 'Doxylamine',
+        img:  doxylamine ,
+        category: 'Antihistamines',
+        quantity: 235,
+        desc: '2024-01-18 00:00:00',
+        price: 7,
+        rating: 2
+    },
+    {
+        id: 18,
+        name: 'Tramadol',
+        img:  tramadol ,
+        category: 'Painkillers',
+        quantity: 245,
+        desc: '2024-01-17 00:00:00',
+        price: 8,
+        rating: 2
+    },
+    {
+        id: 19,
+        name: 'Doxycycline',
+        img:  doxycycline ,
+        category: 'Antibiotics',
+        quantity: 255,
+        desc: '2024-01-16 00:00:00',
+        price: 21,
+        rating: 2
+    },
+    {
+        id: 20,
+        name: 'Famotidine',
+        img:  famotidine ,
+        category: 'Antacids',
+        quantity: 265,
+        desc: '2024-01-15 00:00:00',
+        price: 14,
+        rating: 3
+    },
+    {
+        id: 21,
+        name: 'Oxycodone',
+        img:  oxycodone ,
+        category: 'Painkillers',
+        quantity: 275,
+        desc: '2024-01-14 00:00:00',
+        price: 11,
+        rating: 1
+    },
+    {
+        id: 22,
+        name: 'Mupirocin',
+        img:  mupirocin ,
+        category: 'Antibiotics',
+        quantity: 285,
+        desc: '2024-01-13 00:00:00',
+        price: 18,
+        rating: 1
+    },
+    {
+        id: 23,
+        name: 'Ondansetron',
+        img:  ondansetron ,
+        category: 'Anti-nausea',
+        quantity: 295,
+        desc: '2024-01-12 00:00:00',
+        price: 6,
+        rating: 2
+    },
+    {
+        id: 24,
+        name: 'Ibuprofen',
+        img:  ibuprofen ,
+        category: 'Painkillers',
+        quantity: 305,
+        desc: '2024-01-11 00:00:00',
+        price: 5,
+        rating: 2
+    },
+    {
+        id: 25,
+        name: 'Metoclopramide',
+        img:  metoclopramide ,
+        category: 'Gastrointestinal',
+        quantity: 315,
+        desc: '2024-01-10 00:00:00',
+        price: 13,
+        rating: 5
+    },
+    {
+        id: 26,
+        name: 'Chlorphenamine',
+        img:  chlorphenamine ,
+        category: 'Antihistamines',
+        quantity: 325,
+        desc: '2024-01-09 00:00:00',
+        price: 8,
+        rating: 0
+    },
+    {
+        id: 27,
+        name: 'Nystatin',
+        img:  nystatin ,
+        category: 'Antifungals',
+        quantity: 335,
+        desc: '2024-01-08 00:00:00',
+        price: 7,
+        rating: 4
+    },
+    {
+        id: 28,
+        name: 'Fluconazole',
+        img:  fluconazole ,
+        category: 'Antifungals',
+        quantity: 345,
+        desc: '2024-01-07 00:00:00',
+        price: 20,
+        rating: 2
+    },
+    {
+        id: 29,
+        name: 'Salicylic acid',
+        img:  salicylicAcid ,
+        category: 'Skin Care',
+        quantity: 355,
+        desc: '2024-01-06 00:00:00',
+        price: 11,
+        rating: 1
+    },
+    {
+        id: 30,
+        name: 'Loperamide',
+        img:  loperamide ,
+        category: 'Gastrointestinal',
+        quantity: 365,
+        desc: '2024-01-05 00:00:00',
+        price: 9,
+        rating: 5
+    },
+    {
+        id: 31,
+        name: 'Hydrocortisone',
+        img:  hydrocortisone ,
+        category: 'Topical Steroids',
+        quantity: 375,
+        desc: '2024-01-04 00:00:00',
+        price: 15,
+        rating: 2
+    },
+    {
+        id: 32,
+        name: 'Furosemide',
+        img:  furosemide ,
+        category: 'Diuretics',
+        quantity: 385,
+        desc: '2024-01-03 00:00:00',
+        price: 10,
+        rating: 3
+    },
+    {
+        id: 33,
+        name: 'Prednisone',
+        img:  prednisone ,
+        category: 'Corticosteroids',
+        quantity: 395,
+        desc: '2024-01-02 00:00:00',
+        price: 16,
+        rating: 5
+    },
+    {
+        id: 34,
+        name: 'Clobetasol propionate',
+        img:  clobetasolPropionate ,
+        category: 'Topical Steroids',
+        quantity: 405,
+        desc: '2024-01-01 00:00:00',
+        price: 14,
+        rating: 2
+    },
+    {
+        id: 35,
+        name: 'Fluticasone propionate',
+        img:  fluticasonePropionate ,
+        category: 'Inhaled Steroids',
+        quantity: 415,
+        desc: '2023-12-31 00:00:00',
+        price: 13,
+        rating: 2
+    },
+    {
+        id: 36,
+        name: 'Sildenafil',
+        img:  sildenafil ,
+        category: 'Erectile Dysfunction',
+        quantity: 425,
+        desc: '2023-12-30 00:00:00',
+        price: 17,
+        rating: 0
+    },
+    {
+        id: 37,
+        name: 'Tadalafil',
+        img:  tadalafil ,
+        category: 'Erectile Dysfunction',
+        quantity: 435,
+        desc: '2023-12-29 00:00:00',
+        price: 22,
+        rating: 0
+    },
+    {
+        id: 38,
+        name: 'Metformin',
+        img:  metformin ,
+        category: 'Antidiabetics',
+        quantity: 445,
+        desc: '2023-12-28 00:00:00',
+        price: 9,
+        rating: 2
+    },
+    {
+        id: 39,
+        name: 'Insulin',
+        img:  insulin ,
+        category: 'Antidiabetics',
+        quantity: 455,
+        desc: '2023-12-27 00:00:00',
+        price: 15,
+        rating: 3
+    },
+    {
+        id: 40,
+        name: 'Levothyroxine',
+        img:  levothyroxine ,
+        category: 'Thyroid Medications',
+        quantity: 465,
+        desc: '2023-12-26 00:00:00',
+        price: 11,
+        rating: 2
+    },
+    {
+        id: 41,
+        name: 'Atorvastatin',
+        img:  atorvastatin ,
+        category: 'Statins',
+        quantity: 475,
+        desc: '2023-12-25 00:00:00',
+        price: 14,
+        rating: 3
+    },
+    {
+        id: 42,
+        name: 'Simvastatin',
+        img:  simvastatin ,
+        category: 'Statins',
+        quantity: 485,
+        desc: '2023-12-24 00:00:00',
+        price: 12,
+        rating: 3
+    },
+    {
+        id: 43,
+        name: 'Citalopram',
+        img:  citalopram ,
+        category: 'Antidepressants',
+        quantity: 495,
+        desc: '2023-12-23 00:00:00',
+        price: 18,
+        rating: 2
+    },
+    {
+        id: 44,
+        name: 'Sertraline',
+        img:  sertraline ,
+        category: 'Antidepressants',
+        quantity: 505,
+        desc: '2023-12-22 00:00:00',
+        price: 16,
+        rating: 5
+    },
+    {
+        id: 45,
+        name: 'Escitalopram',
+        img:  escitalopram ,
+        category: 'Antidepressants',
+        quantity: 515,
+        desc: '2023-12-21 00:00:00',
+        price: 20,
+        rating: 1
+    },
+    {
+        id: 46,
+        name: 'Venlafaxine',
+        img:  venlafaxine ,
+        category: 'Antidepressants',
+        quantity: 525,
+        desc: '2023-12-20 00:00:00',
+        price: 19,
+        rating: 4
+    },
+    {
+        id: 47,
+        name: 'Duloxetine',
+        img:  duloxetine ,
+        category: 'Antidepressants',
+        quantity: 535,
+        desc: '2023-12-19 00:00:00',
+        price: 22,
+        rating: 1
+    },
+    {
+        id: 48,
+        name: 'Fluoxetine',
+        img:  fluoxetine ,
+        category: 'Antidepressants',
+        quantity: 545,
+        desc: '2023-12-18 00:00:00',
+        price: 18,
+        rating: 4
+    },
+    {
+        id: 49,
+        name: 'Bupropion',
+        img:  bupropion ,
+        category: 'Antidepressants',
+        quantity: 555,
+        desc: '2023-12-17 00:00:00',
+        price: 21,
+        rating: 4
+    },
+    {
+        id: 50,
+        name: 'Mirtazapine',
+        img:  mirtazapine ,
+        category: 'Antidepressants',
+        quantity: 565,
+        desc: '2023-12-16 00:00:00',
+        price: 17,
+        rating: 2
+    },
+    {
+        id: 51,
+        name: 'Trazodone',
+        img:  trazodone ,
+        category: 'Antidepressants',
+        quantity: 575,
+        desc: '2023-12-15 00:00:00',
+        price: 20,
+        rating: 4
+    },
+    {
+        id: 52,
+        name: 'Amitriptyline',
+        img:  amitriptyline ,
+        category: 'Antidepressants',
+        quantity: 585,
+        desc: '2023-12-14 00:00:00',
+        price: 16,
+        rating: 5
+    },
+    {
+        id: 53,
+        name: 'Nortriptyline',
+        img:  nortriptyline ,
+        category: 'Antidepressants',
+        quantity: 595,
+        desc: '2023-12-13 00:00:00',
+        price: 18,
+        rating: 1
     }
     
 ]
 
+// cephalexin
+// pantoprazole
+// hydrocodone
+// clarithromycin
+// doxylamine
+// tramadol
+// doxycycline
+// famotidine
+// oxycodone
+// mupirocin
+// ondansetron
+// ibuprofen
+// metoclopramide
+// chlorphenamine
+// nystatin
+// fluconazole
+// salicylic acid
+// loperamide
+// hydrocortisone
+// furosemide
+// prednisone
+// clobetasol propionate
+// fluticasone propionate
+// sildenafil
+// tadalafil
+// metformin
+// insulin
+// levothyroxine
+// atorvastatin
+// simvastatin
+// citalopram
+// sertraline
+// escitalopram
+// venlafaxine
+// duloxetine
+// fluoxetine
+// bupropion
+// mirtazapine
+// trazodone
+// amitriptyline
+// nortriptyline
